415d9d3a962b3c80fbb1d1f2fc32543d36832fe5
luiar <luiar@ANTONIO>
2022-09-30 13:25:37 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Circle
+{
+    class Circle
+    {
+        float Pi = 3.141f;
+        public int Radius;
+        public int circum;
+        public void getarea()
+        {
+            Console.WriteLine("Radien på cirkeln är " + Radius*Radius+Pi);
+        }
+        public void getcircum()
+        {
+            Console.WriteLine("Omkretsen på circeln är " + 2 * Pi * Radius);
+        }
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Circle circle1 = new Circle();
+            circle1.Radius = 100;
+
+            circle1.getarea();
+            circle1.getcircum();
+           
+        }
+    }
+}
